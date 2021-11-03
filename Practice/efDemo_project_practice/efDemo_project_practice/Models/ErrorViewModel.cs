decbc4ec7e7585e940cc891bf64e90c480d210ab
Balazs Rigo <rigo.balazs86@gmail.com>
2021-11-03 12:51:14 +0100

    Projects are created

---
+++ Practice/efDemo_project_practice/efDemo_project_practice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace efDemo_project_practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
